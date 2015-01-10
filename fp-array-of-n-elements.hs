da1ad10fa6336fe7c084056ac64b0b4f51256644
codeJanitor <nbrown@cedarville.edu>
2015-01-09 19:44:15 -0800

    Added new exercises.

---
+++ fp-array-of-n-elements.hs
@@ -0,0 +1,16 @@
+{- 
+    = Hacker Rank: Array of N Elements 
+    <https://www.hackerrank.com/challenges/fp-array-of-n-elements>
+    
+    Author: Nathan Brown <nbrown@cedarville.edu>
+    Date: 12/30/14
+    
+    Description: Generate an array of length 'n', 
+    composed of arbitrary integers.
+    
+    Quick solution: fn n = replicate n 1
+    For a more accurate solution, see below.
+-}
+
+
+fn n = 
