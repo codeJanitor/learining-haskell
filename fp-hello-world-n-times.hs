6279615d14e3dad99365bd354f096adf04836b05
Nathan Brown <nathan.brown@alleluu.com>
2015-01-20 12:16:12 -0800

    Added new Haskell exercises.

---
+++ fp-hello-world-n-times.hs
@@ -0,0 +1,9 @@
+hello_worlds n 
+    | n > 0 = do
+        putStrLn "Hello World"
+        hello_worlds (n-1)
+    | otherwise = return ()
+
+main = do
+    n <- readLn :: IO Int
+    hello_worlds n
---
+++ fp-hello-world.hs
@@ -0,0 +1,7 @@
+-- Enter your code here. Read input from STDIN.
+-- Print output to STDOUT.
+
+hello_world = putStrLn "Hello World"
+
+main = do
+    hello_world
